e477f1629c38d872d0266a757c55376535dc91f5
amirulislambd <amirulislambd313@gmail.com>
2026-03-08 00:55:28 +0600

    updated error logic of login input

--- js/login.js
+++ js/login.js
@@ -22,21 +22,30 @@ document.getElementById("signInBtn").addEventListener("click", () => {
     defaultUser.innerText =''
   const userNameValue = userName.value;
   const passwordValue = password.value;
-  if (userNameValue != "admin") {
+  if(userNameValue.trim() ===''){
+    defaultUser.innerHTML = `<i class="fa-regular fa-circle-xmark "></i>Please enter <span class="text-green-500">'admin'</span> your user name`;
+    defaultUser.classList.add('text-red-500','animate-bounce') 
+    return;
+  }else if (userNameValue != "admin") {
    defaultUser.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Wrong user name Please enter 'admin'`;
    defaultUser.classList.add('text-red-500','animate-bounce') 
     return;
-  }else{
+  }
+  else{
       defaultUser.classList.remove('text-red-500','animate-bounce','text-purple-500') 
       defaultUser.classList.add('text-green-500') 
       defaultUser.innerHTML =`<i class="fa-regular fa-circle-check"></i> Correct Your user name `;
       
   }
-   if (passwordValue != "admin123") {
+  if(passwordValue.trim()===''){
+    defaultPassword.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Please enter <span class="text-green-500">'admin123'</span>  your password`;
+    defaultPassword.classList.add('text-red-500','animate-bounce') 
+    return;
+  }else if (passwordValue != "admin123") {
    defaultPassword.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Wrong Your Password Please enter 'admin123'`;
    defaultPassword.classList.add('text-red-500','animate-bounce') 
     return;
-  }else{
+  } else{
       defaultPassword.classList.remove('text-red-500','animate-bounce','text-purple-500') 
       defaultPassword.classList.add('text-green-500') 
       defaultPassword.innerHTML =`<i class="fa-regular fa-circle-check"></i> Correct Your Password `;
